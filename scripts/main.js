3cf147f97f43f255aa563ba89d1e4dfa99d2940d
Albert Kemp <albertkemp@3360L-144597-M.local>
2024-05-15 19:12:35 +1000

    added blog post functionality

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$('.post').attr('class', 'hidden');
+$('.post').each(function() {
+    var $post = $(this);
+    var $readMoreButton = $('<button>').text('Read More');
+    
+    $readMoreButton.on('click', function() {
+        $post.toggleClass('hidden visible');
+    });
+    
+    $post.append($readMoreButton);
+});
